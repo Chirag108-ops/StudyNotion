b8b2dfc1e77f000610428e3fedf92ccda57c9640
Chirag Goyal <chiraggoyal@Chirags-MacBook-Air.local>
2023-07-17 20:52:24 +0530

    Url updation in Reset Password

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -27,7 +27,7 @@ exports.resetPasswordToken = async (req, res) => {
             new: true
         })
 
-        const url = `http://localhost:3000/update-password/${token}`
+        const url = `https://study-notion-kappa.vercel.app/update-password/${token}`
         const response = await sendMail(email, 'Reset Password - StudyNotion', `Password Reset Link - ${url}`)
         return res.status(200).json({
             success: true,
